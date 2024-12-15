5c1a09e00a80fd8297b22f485890847c7c8c945d
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-12-15 18:54:40 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -13,26 +13,30 @@
         let pkgs = mkPkgs system;
         in {
           default = pkgs.stdenv.mkDerivation {
-            name = "ansi";
+            pname = "ansi";
             version = "1.0.0";
             src = ./.;
-            buildInputs = [ pkgs.zig ];
+            buildInputs = with pkgs; [ zig ];
             buildPhase = ''
-              export LIBRARY_PATH=/usr/lib
-              export DYLD_LIBRARY_PATH=/usr/lib
+              ${pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isDarwin ''
+                # On Darwin the executable needs to link to libSystem found in /usr/lib/
+                export LIBRARY_PATH=/usr/lib
+                export DYLD_LIBRARY_PATH=/usr/lib
+              ''}
               cacheDir=$(mktemp -d)
               zig build --global-cache-dir $cacheDir
             '';
-            installPhase = ''
-              mkdir -p $out/bin
-              cp zig-out/bin/ansi $out/bin
-            '';
-            meta = with pkgs.lib; {
-              description = "A cheatsheet for ansi escape codes";
+            installPhase = "install -D zig-out/bin/ansi -t $out/bin";
+            meta = {
+              description = "A quick reference guide for ANSI escape codes";
+              longDescription = ''
+                A handy cheatsheet for quickly looking up ANSI escape codes.
+                Perfect for developers working with terminal color codes and text formatting.
+              '';
               homepage = "https://github.com/NewDawn0/ansi";
-              license = licenses.mit;
-              maintainers = [ NewDawn0 ];
-              platforms = platforms.all;
+              license = pkgs.lib.licenses.mit;
+              maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+              platforms = pkgs.lib.platforms.all;
             };
           };
         });
